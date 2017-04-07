6320b6cf013b1cf828e1e0a58d67153747b49689
maciula <maciek.cel12@gmail.com>
2017-04-07 10:30:10 +0200

    custom style for sign in view

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by itelab_maciula on 07.04.2017.
+//  Copyright © 2017 itelab_maciula. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
